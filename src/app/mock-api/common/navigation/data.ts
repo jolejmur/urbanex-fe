cfe65068c63a4a08c44982e32dfb1b0d254ffd3c
Urbanex <urbanex@Mac-mini-de-Jorge-Mucarcel.local>
2025-05-03 17:01:07 -0400

    first commit

---
+++ src/app/mock-api/common/navigation/data.ts
@@ -0,0 +1,67 @@
+/* eslint-disable */
+import { FuseNavigationItem } from '@fuse/components/navigation';
+
+export const defaultNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    },
+    {
+        id   : 'comunidades',
+        title: 'Comunidades',
+        type : 'basic',
+        icon : 'heroicons_outline:users',
+        link : '/comunidades/contacts'
+    }
+];
+export const compactNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    },
+    {
+        id   : 'comunidades',
+        title: 'Comunidades',
+        type : 'basic',
+        icon : 'heroicons_outline:users',
+        link : '/comunidades/contacts'
+    }
+];
+export const futuristicNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    },
+    {
+        id   : 'comunidades',
+        title: 'Comunidades',
+        type : 'basic',
+        icon : 'heroicons_outline:users',
+        link : '/comunidades/contacts'
+    }
+];
+export const horizontalNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    },
+    {
+        id   : 'comunidades',
+        title: 'Comunidades',
+        type : 'basic',
+        icon : 'heroicons_outline:users',
+        link : '/comunidades/contacts'
+    }
+];
---
+++ src/app/modules/admin/apps/comunidades/list/list.component.ts
@@ -0,0 +1,1 @@
+// No hay código que renombrar, el documento está vacío
